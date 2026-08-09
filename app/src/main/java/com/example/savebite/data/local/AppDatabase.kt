0f02ed8ab7fbf0d828f880ed568c983e6969b14a
Jonathan <jonathannjy-pm24@student.tarc.edu.my>
2026-08-09 16:02:03 +0800

    Updated AppDataBase format

--- app/src/main/java/com/example/savebite/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/savebite/data/local/AppDatabase.kt
@@ -4,37 +4,54 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.savebite.data.local.dao.InventoryDao
 import com.example.savebite.data.local.dao.StorageDao
-import com.example.savebite.data.local.dao.UserDao
 import com.example.savebite.model.Inventory
 import com.example.savebite.model.Storage
-import com.example.savebite.model.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-// Room database
-@Database(entities = [User::class, Inventory::class, Storage::class], version = 2)
+@Database(
+    entities = [Inventory::class, Storage::class],
+    version = 2,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun userDao(): UserDao
+
     abstract fun inventoryDao(): InventoryDao
     abstract fun storageDao(): StorageDao
 
-    // Singleton instance of the database
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase {
-            if (INSTANCE == null) {
-                INSTANCE = Room.databaseBuilder(
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "savebite_database"
                 )
                     .fallbackToDestructiveMigration()
+                    .addCallback(object : RoomDatabase.Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            INSTANCE?.let { database ->
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    val storageDao = database.storageDao()
+                                    storageDao.insertStorage(Storage("Pantry"))
+                                    storageDao.insertStorage(Storage("Refrigerator"))
+                                    storageDao.insertStorage(Storage("Freezer"))
+                                }
+                            }
+                        }
+                    })
                     .build()
+                INSTANCE = instance
+                instance
             }
-            return INSTANCE!!
         }
     }
 }
